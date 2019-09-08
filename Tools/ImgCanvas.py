2e3334f9c5856c18a31ccd3d322eb3727fd81f67
Hui <hui.li@rvbust.com>
2019-09-08 20:35:58 +0800

    add imgcanvas as the image inspector

---
+++ Tools/ImgCanvas.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Copyright (c) RVBUST, Inc - All rights reserved.
+
+
+import numpy as np
+from PIL import Image 
+from matplotlib import pyplot as plt
+import matplotlib.patches as patches
+
+fig, ax = plt.subplots(1)
+
+class ImgCanvas(object):
+    def __init__(self, imgorname):
+        if type(imgorname) is str:            
+            img = np.array(Image.open(imgorname), dtype = np.uint8)
+        else:
+            img = imgorname
+        plt.imshow(img)
+
+    def Plot(self, *args, scalex=True, scaley=True, data=None, **kwargs):
+        plt.plot(*args, scalex=scalex, scaley=scaley, data=data, **kwargs)
+        
+    def Point(self, x, y, size = 1, color = (1,0,0)): 
+        if isinstance(x, (list, tuple, np.ndarray)):
+            xs = x
+            ys = y
+        else:
+            xs = [x]
+            ys = [y]            
+        plt.plot(xs, ys, marker='.', markersize=size, color=color)
+        
+    def Line(self, a, b, color = (1,0,0), linewidth=1):
+        if isinstance(a[0], (list, tuple)):
+            la = a
+            lb = b
+        else:
+            la = [a]
+            lb = [b]
+        for pa, pb in zip(la, lb):
+            line = plt.Line2D(pa, pb, lw = linewidth, color = color)
+            plt.gca().add_line(line)
+        
+    def Circle(self, center, radius, color = (1,0,0, 0.5), fill = True):
+        circle = plt.Circle(center, radius = radius, color = color, fill = fill)
+        plt.gca().add_patch(circle)
+        
+    def Rectangle(self, xy, width, height, color = (1,0,0,0.5), fill = True):
+        rect = plt.Rectangle(xy, width, height, color = color, fill = fill)
+        plt.gca().add_patch(rect)
+
+    def Arrow(self, x, y, dx, dy, head_width=3, head_length=8, **kwargs):
+        plt.arrow(x, y, dx, dy, head_width=head_width, head_length=head_length, **kwargs)
+        
+    def Ellipse(self, xy, width, height, angle = 0, **kwargs):
+        ell = patches.Ellipse(xy, width, height, angle=angle, **kwargs)
+        plt.gca().add_patch(ell)
+    
+    def Polygon(self, points, closed = True, fill = False, color = (1.0,0,0)):
+        poly = plt.Polygon(points, closed = closed, fill = fill, edgecolor = color)
+        plt.gca().add_patch(poly)
+        
+    def Show(self):
+        plt.show()
+
+def Test():
+    import sys 
+    c = ImgCanvas(sys.argv[1])
+    c.Plot(np.arange(300), 30 + 10*np.sin(np.arange(300)), linewidth=1, color = (1,0,0,0.5))
+    c.Point(range(200), range(200))
+    c.Line([(100, 100), (200, 100)], [(100, 200.4), (200, 300)])
+    c.Line((200, 300), (210, 400))
+    c.Circle((100, 100), 30, color = (1.0, 0, 0, 0.5))
+    c.Rectangle((150,200), width = 30, height = 50, color = (0,1.0,0,0.5))
+    c.Polygon([(100, 100), (300, 90), (400, 300), (88, 99)], color = (0,1,0))
+    c.Ellipse((200, 200), width = 40, height = 30, angle = 30, color = (1.0,0,0), fill=False)
+    c.Arrow(10, 10, 300, 130, head_width=3, head_length=8, color = (1.0,0,0, 0.5))
+    c.Show();
+
+if __name__ == '__main__':
+    Test()
+    
